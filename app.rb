99ab3caafd61e9e3dae2fb7c86cd6776149b89a7
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-02-15 16:41:07 -0500

    works

---
+++ app.rb
@@ -0,0 +1,50 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+
+get "/" do
+
+	@title = "What is your name?"
+
+	erb :get_name
+end
+
+post "/name" do
+
+	@title = "What is your location?"
+	@user_name = params[:name]
+
+	erb :get_location
+end
+
+post "/location" do
+
+	@title = "What are your three favorite numbers?(Seperate each by a comma)?"
+	@user_name = params[:name]
+	@user_location = params[:location]
+
+	erb :get_numbers
+end
+
+post "/numbers" do
+
+	@title = "What is your age?"
+	@user_name = params[:name]
+	@user_location = params[:location]
+	@user_numbers = params[:numbers]
+
+	erb :get_age
+end
+
+post "/age" do
+
+	@user_name = params[:name]
+	@user_location = params[:location]
+	@user_numbers = params[:numbers]
+	@user_age = params[:age]
+
+	sum = 0
+	@user_numbers.split(",").each_with_index{|n|sum += n.to_i}
+	g_or_l = sum.to_i > @user_age.to_i ? "greater" : "less"
+
+	@title = "Hey #{@user_name}, your age is #{@user_age}, your location is #{@user_location},your three favorite numbers are #{@user_numbers} and their sum is #{sum}, and it is #{g_or_l} than your age."
+end
