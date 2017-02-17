b8f3311d7e825a42bf9f4663a897e2ad5e665018
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-02-16 19:13:51 -0500

    redirect working using sessions

---
+++ app_redirect.rb
@@ -0,0 +1,62 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+
+use Rack::Session::Cookie, :key => "rack.session"
+
+get "/" do
+
+	@title = "What is your name?"
+
+	erb :get_name
+end
+
+post "/name" do 
+
+	session[:user_name] = params[:name]
+	redirect ("/location?name=" + session[:user_name])
+end
+
+get "/location" do
+
+	@title = "What is your location?"
+	erb :get_location
+end
+
+post "/location" do
+
+	session[:user_location] = params[:location]
+	redirect ("/numbers?name=#{session[:user_name]}location=" + session[:user_location])
+end
+
+get "/numbers" do
+
+	@title = "What are your three favorite numbers?(Please enter numbers separated by a comma)"
+	erb :get_numbers
+end
+
+post "/numbers" do
+
+	session[:user_numbers] = params[:numbers]
+	redirect "/age?name=#{session[:user_name]}location=#{session[:user_location]}numbers=" + session[:user_numbers]
+end
+
+get "/age" do
+
+	@title = "What is your age?"
+	erb :get_age
+end
+
+post "/age" do
+
+	session[:user_age] = params[:age]
+	redirect "/final?name=#{session[:user_name]}location=#{session[:user_location]}numbers=#{session[:user_numbers]}age=" + session[:user_age]
+end
+
+get "/final" do
+
+	sum = 0
+	session[:user_numbers].split(",").each_with_index{|n|sum += n.to_i}
+	g_or_l = sum.to_i > @user_age.to_i ? "greater" : "less"
+
+	@title = "Hey #{session[:user_name]}, your age is #{session[:user_age]}, your location is #{session[:user_location]},your three favorite numbers are #{session[:user_numbers]} and their sum is #{sum}, and it is #{g_or_l} than your age."
+end
